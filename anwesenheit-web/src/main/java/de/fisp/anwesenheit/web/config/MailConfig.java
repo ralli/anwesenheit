b49634cc1cc5032185bf6284639142a4cce967c0
feffi <feffi1@gmail.com>
2013-05-05 20:48:53 +0200

    Revert "Revert "corrected log messages""
    This reverts commit c513f66dc2670e4a8d56b11412111b5236e3a747.

--- anwesenheit-web/src/main/java/de/fisp/anwesenheit/web/config/MailConfig.java
+++ anwesenheit-web/src/main/java/de/fisp/anwesenheit/web/config/MailConfig.java
@@ -1,5 +1,7 @@
 package de.fisp.anwesenheit.web.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import de.fisp.anwesenheit.core.service.MailService;
@@ -7,7 +9,11 @@ import de.fisp.anwesenheit.core.service.impl.DummyMailServiceImpl;
 
 @Configuration
 public class MailConfig {
+	
+	private static final Logger log = LoggerFactory.getLogger(MailConfig.class);
+	
   public MailService mailService() {
+  	log.info("Register mail service...");
     return new DummyMailServiceImpl();
   }
 }
