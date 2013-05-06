1710bc575574ae6ca2a0075a3d894cf1dc38b91f
feffi <feffi1@gmail.com>
2013-05-06 08:25:12 +0200

    added developer bios ;-) moved to Java/1.7 upgraded libraries to... 	springframework/3.2.2.RELEASE 	hibernate/4.2.1.Final 	hibernate-validator/5.0.0.Final 	junit/4.11 	cglib-nodep/3.0 	mysql-connector-java/5.1.25 	aspectjrt/1.7.2 	commons-codec/1.8 	servlet-api/3.0-alpha-1 	jsp-api/2.2.1-b03 	jackson-mapper-asl/1.9.11
    added abstract for pom version management
    added maven build metrics
    added javadoc generator
    added maven-enforcer-plugin
    added dashboard-maven-plugin

--- anwesenheit-core/src/main/java/de/fisp/anwesenheit/core/config/TxConfig.java
+++ anwesenheit-core/src/main/java/de/fisp/anwesenheit/core/config/TxConfig.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
