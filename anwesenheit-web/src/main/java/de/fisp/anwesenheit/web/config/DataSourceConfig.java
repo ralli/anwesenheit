774ce9e1777ba964de7fbdecb20d38b293594df3
Ralph Juhnke <r_juhnke@gmx.de>
2013-05-25 01:06:49 +0200

    JNDI-Datenquelle

--- anwesenheit-web/src/main/java/de/fisp/anwesenheit/web/config/DataSourceConfig.java
+++ anwesenheit-web/src/main/java/de/fisp/anwesenheit/web/config/DataSourceConfig.java
@@ -2,40 +2,46 @@ package de.fisp.anwesenheit.web.config;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 @Configuration
+@PropertySource("classpath:/anwesenheit.properties")
 public class DataSourceConfig {
-
   private static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
+  @Autowired
+  private org.springframework.core.env.Environment env;
 
+//  @Bean
+//  public DataSource dataSource() {
+//    log.info("Register data source...");
+//    BasicDataSource datasource = new BasicDataSource();
+//    datasource.setUrl("jdbc:mysql://localhost/anwesenheit");
+//    datasource.setDriverClassName("com.mysql.jdbc.Driver");
+//    datasource.setUsername("anwesenheit");
+//    datasource.setPassword("anwesenheit");
+//    datasource.setInitialSize(5);
+//    datasource.setMaxActive(10);
+//    datasource.setMaxIdle(0);
+//    datasource.setTimeBetweenEvictionRunsMillis(1000 * 60 * 10);
+//    datasource.setMinEvictableIdleTimeMillis(1000 * 60 * 15);
+//    return datasource;
+//  }
   @Bean
-  public DataSource dataSource() {
+  public DataSource dataSource() throws Exception {
     log.info("Register data source...");
-    BasicDataSource datasource = new BasicDataSource();
-    datasource.setUrl("jdbc:mysql://localhost/anwesenheit");
-    datasource.setDriverClassName("com.mysql.jdbc.Driver");
-    datasource.setUsername("anwesenheit");
-    datasource.setPassword("anwesenheit");
-    datasource.setInitialSize(5);
-    datasource.setMaxActive(10);
-    datasource.setMaxIdle(0);
-    datasource.setTimeBetweenEvictionRunsMillis(1000 * 60 * 10);
-    datasource.setMinEvictableIdleTimeMillis(1000 * 60 * 15);
-    return datasource;
+    DataSource ds;
+    JndiObjectFactoryBean jndiFactory = new JndiObjectFactoryBean();
+    String  jndiDatasourceName = env.getProperty("jdbc.jndiDatasourceName");
+    log.info("jndiDatasourceName={}", jndiDatasourceName);
+    jndiFactory.setJndiName(jndiDatasourceName);
+    jndiFactory.afterPropertiesSet();
+    ds = (DataSource) jndiFactory.getObject();
+    return ds;
   }
-  //    @Bean
-//    public DataSource dataSource() throws Exception {
-//        DataSource ds;
-//
-//        JndiObjectFactoryBean jndiFactory = new JndiObjectFactoryBean();
-//        jndiFactory.setJndiName("java:jboss/datasources/PetclinicDS");
-//        jndiFactory.afterPropertiesSet();
-//        ds = (DataSource) jndiFactory.getObject();
-//        return ds;
-//    }
 }
