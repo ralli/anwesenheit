ea8410a0a835790a46b80378926bf1a5181ebfda
Ralph Juhnke <r_juhnke@gmx.de>
2013-05-27 23:54:21 +0200

    Unit-Tests heile gemacht

--- anwesenheit-core/src/test/java/de/fisp/anwesenheit/core/TestConfig.java
+++ anwesenheit-core/src/test/java/de/fisp/anwesenheit/core/TestConfig.java
@@ -1,10 +1,16 @@
 package de.fisp.anwesenheit.core;
 
+import de.fisp.anwesenheit.core.service.MailService;
+import de.fisp.anwesenheit.core.service.impl.DummyMailServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"de.fisp.anwesenheit.core"})
 public class TestConfig {
-
+  @Bean
+  public MailService mailService() {
+    return new DummyMailServiceImpl();
+  }
 }
