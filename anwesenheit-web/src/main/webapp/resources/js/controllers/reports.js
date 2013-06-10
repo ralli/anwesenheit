c0fd4e42250cbad5361b4edae69006ffdc539e22
Ralph Juhnke <r_juhnke@gmx.de>
2013-06-11 00:07:42 +0200

    Vorbelegung des Reporting-Datums auf Vormonat gesetzt

--- anwesenheit-web/src/main/webapp/resources/js/controllers/reports.js
+++ anwesenheit-web/src/main/webapp/resources/js/controllers/reports.js
@@ -1,34 +1,31 @@
 // Encoding: UTF-8
 
 (function () {
-    'use strict';
+  'use strict';
 
-    /* global angular: false, $: false, _: false, toastr: false */
+  /* global angular: false, $: false, _: false, toastr: false */
 
-    var app = angular.module("app.ctrl.reports", [ "ngResource", "ui", "ui.bootstrap", "app.services", "app.helpers"]);
+  var app = angular.module("app.ctrl.reports", [ "ngResource", "ui", "ui.bootstrap", "app.services", "app.helpers"]);
 
-    app.controller("ReportsCtrl", ["$scope", "$filter", function($scope, $filter) {
-        $scope.monate = [
-            {"num": "01", "name": "Januar"},
-            {"num": "02", "name": "Februar"},
-            {"num": "03", "name": "März"},
-            {"num": "04", "name": "April"},
-            {"num": "05", "name": "Mai"},
-            {"num": "06", "name": "Juni"},
-            {"num": "07", "name": "Juli"},
-            {"num": "08", "name": "August"},
-            {"num": "09", "name": "September"},
-            {"num": "10", "name": "Oktober"},
-            {"num": "11", "name": "November"},
-            {"num": "12", "name": "Dezember"}
-        ];
+  app.controller("ReportsCtrl", ["$scope", "$filter", function ($scope, $filter) {
+    $scope.monate = [
+      {"num": "01", "name": "Januar"},
+      {"num": "02", "name": "Februar"},
+      {"num": "03", "name": "März"},
+      {"num": "04", "name": "April"},
+      {"num": "05", "name": "Mai"},
+      {"num": "06", "name": "Juni"},
+      {"num": "07", "name": "Juli"},
+      {"num": "08", "name": "August"},
+      {"num": "09", "name": "September"},
+      {"num": "10", "name": "Oktober"},
+      {"num": "11", "name": "November"},
+      {"num": "12", "name": "Dezember"}
+    ];
 
-        var d = new Date();
-        $scope.monat = $filter("date")(d, "MM");
-        $scope.jahr = d.getFullYear();
-        $scope.datumFun = function() {
-            return new Date("" + $scope.jahr + "-" + $scope.monat + "-1");
-        };
-        $scope.datum = $scope.datumFun();
-    }]);
+    var d = new Date();
+    d.setMonth(d.getMonth() - 1);
+    $scope.monat = $filter("date")(d, "MM");
+    $scope.jahr = d.getFullYear();
+  }]);
 }).call(this);
